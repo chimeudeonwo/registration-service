c77e537b6014afb6400f7b8dff704d5b4146f452
Patrick <onyebuchiudeh1@yahoo.com>
2023-04-02 11:46:35 +0200

    add frontend setup

---
+++ registration-service-frontend/registration-service-frontend/src/RegistrationApp.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './RegistrationApp.scss';
+
+function RegistrationApp() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/RegistrationApp.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default RegistrationApp;
