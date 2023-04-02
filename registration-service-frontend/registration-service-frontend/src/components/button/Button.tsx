e7de33a56e314f80c74d08495a4308ed79ec3c37
Patrick <onyebuchiudeh1@yahoo.com>
2023-04-02 19:59:39 +0200

    refactor

--- registration-service-frontend/registration-service-frontend/src/components/button/Button.tsx
+++
@@ -1,6 +0,0 @@
-
-export const Button = () => {
-    return (
-        <button name={"btn"} onClick={() => console.log('btn clicked')} />
-    )
-}
--- registration-service-frontend/registration-service-frontend/src/modules/errorPage/ErrorPage.tsx
+++
@@ -1,7 +0,0 @@
-export const ErrorPage = () => {
-    return (
-        <div>
-            <p>404 page not found</p>
-        </div>
-    )
-}
